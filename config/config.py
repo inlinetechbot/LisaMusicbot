28aa05c9a58fb9e8156510ea1969901e7940728a
inlinetechbot <inlinetechbot@gmail.com>
2025-12-18 21:35:40 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -57,7 +57,7 @@ OWNER_ID = list(
 # make your bots privacy from telegra.ph and put your url here
 
 PRIVACY_LINK = getenv(
-    "PRIVACY_LINK", "https://telegra.ph/Privacy-Policy-for-VenomMusic-01-02"
+    "PRIVACY_LINK", "https://telegra.ph/Privacy-Policy-for-LisaMusic-12-18"
 )
 
 
